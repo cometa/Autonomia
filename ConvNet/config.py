fbe6b54e71e08065b1fc5b1cc12d32e83842ff12
Marco Graziano <marco@visiblenergy.com>
2016-12-31 16:01:46 -0800

    crop acquired video for prediction

--- ConvNet/config.py
+++ ConvNet/config.py
@@ -1,12 +1,12 @@
 class DataConfig(object):
-    img_height = 240
+    img_height = 150
     img_width = 320
     num_channels = 1
     num_buckets = 15
     
 class TrainConfig(DataConfig):
-    model_name = "prelu"
-    batch_size = 32
+    model_name = "relu"
+    batch_size = 64
     num_epoch = 30
 
 class TestConfig(TrainConfig):
